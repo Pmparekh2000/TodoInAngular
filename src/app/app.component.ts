04507a5d9cd0cc1a91ab7f239e035312af7bd4e0
Pmparekh2000 <pmp.tech2000@gmail.com>
2020-04-10 19:38:13 +0530

    Functionality added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from "./todo";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Todo';
+
+  todoValue:string;
+  list: Todo[];
+
+  ngOnInit(){
+    this.list = [];
+    this.todoValue = "";
+  }
+
+  addItem(){
+    if (this.todoValue !== "") {
+      const newItem: Todo = {
+        id:Date.now(),
+        value:this.todoValue,
+        isDone:false
+      };
+      this.list.push(newItem);
+    }else{
+      alert("Please put something");
+    }
+    this.todoValue = "";
+  }
+
+  deleteItem(id:number){
+    this.list = this.list.filter(item => item.id !== id);
+  }
 }
